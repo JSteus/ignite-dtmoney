4b5a2ab4300f72b4bf0f81f5042977c698568937
Mateus <mateus2021r@outlook.com>
2021-07-19 01:10:21 -0300

    Projeto finalizado. De volta a ativa

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { GlobalStyles } from "./styles/global";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+export function App() {
+  const [isNewTransactionModalOpen, seTisNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    seTisNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    seTisNewTransactionModalOpen(false);
+  }
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyles />
+    </TransactionsProvider>
+  );
+}
---
+++ src/hooks/useTransactions.tsx
@@ -0,0 +1,67 @@
+import { ReactNode, createContext, useEffect, useState, useContext } from "react";
+import { api } from "../services/api";
+
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
+// interface TransactionInput {
+//   title: string;
+//   amount: number;
+//   type: string;
+//   category: string;
+// }
+
+// type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category'>;
+
+type TransactionInput = Omit<Transaction, "id" | "createdAt">;
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
+
+const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post("/transactions", {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
+  }
+
+  useEffect(() => {
+    api
+      .get("transactions")
+      .then(({ data }) => setTransactions(data.transactions));
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context
+}
